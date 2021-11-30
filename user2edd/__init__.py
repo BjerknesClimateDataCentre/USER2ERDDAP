9694072001008b1abcb2e39f67593743a0e8fece
Julien Paul <julien.paul@ymail.com>
2021-11-30 14:54:16 +0100

    Bump version: 0.1.0 → 0.2.0

--- user2edd/__init__.py
+++ user2edd/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Julien Paul"""
 __email__ = "julien.paul@uib.no"
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.2.0'"
 
 # where configuration files are stored
 __pkg_cfg__ = f"{__package__}.cfg"
